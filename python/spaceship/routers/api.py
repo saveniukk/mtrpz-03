e5da2aaae479cca1b74c9fb4347670a270246976
Myroslava Saveniuk <saveniukf@gmail.com>
2025-05-22 11:57:11 +0300

    added multiply-matrices endpoint

--- python/spaceship/routers/api.py
+++ python/spaceship/routers/api.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter
+import numpy as np
 
 router = APIRouter()
 
@@ -6,3 +7,16 @@ router = APIRouter()
 @router.get('')
 def hello_world() -> dict:
     return {'msg': 'Hello, World!'}
+
+@router.get('/multiply-matrices')
+def multiply_matrices():
+    matrix_a = np.random.randint(0, 10, size=(10, 10)).tolist()
+    matrix_b = np.random.randint(0, 10, size=(10, 10)).tolist()
+
+    product = (np.dot(matrix_a, matrix_b)).tolist()
+
+    return {
+        'matrix_a': matrix_a,
+        'matrix_b': matrix_b,
+        'product': product
+    }
